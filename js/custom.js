01dede256b8037241ef7ccf46a925cbf2e217ef6
eduardo7mejia <mejiaeduardo144@gmail.com>
2020-02-17 11:36:45 -0600

    Agregamos proyecto

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+
+/******************************************************************************************************************/
+/** PRETTY PHOTO                                                                                                  */
+/******************************************************************************************************************/
+var prettyPhoto = $("a[data-rel^='prettyPhoto']");
+prettyPhoto.prettyPhoto({
+    animationSpeed: 'slow',
+    social_tools: false,
+    theme: 'light_square',
+    slideshow: false,
+    overlay_gallery: false,
+    deeplinking: false,
+    show_title: true
+});
+
